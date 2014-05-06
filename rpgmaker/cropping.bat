3ba6c89886b142a322aec6966228117b818dc86f
RainingChain <rainingchain@gmail.com>
2014-05-06 15:18:29 -0400

    enelver rpgmaker

--- rpgmaker/cropping.bat
+++
@@ -1,8 +0,0 @@
-convert a.png -crop 96x128+0+0 a0.png
-convert a.png -crop 96x128+96+0 a1.png
-convert a.png -crop 96x128+192+0 a2.png
-convert a.png -crop 96x128+288+0 a3.png
-convert a.png -crop 96x128+0+128 a4.png
-convert a.png -crop 96x128+96+128 a5.png
-convert a.png -crop 96x128+192+128 a6.png
-convert a.png -crop 96x128+288+128 a7.png
